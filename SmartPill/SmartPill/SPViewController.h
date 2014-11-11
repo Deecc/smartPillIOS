2d3b9f647d0040c8da59e4c06e8297ad8f5029d9
Dennis da Silva Nunes <nunesdennis@MacBook-Air-de-Dennis.local>
2014-11-11 10:04:25 -0300

    Commit start project.

---
+++ SmartPill/SmartPill/SPViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPViewController.h
+//  SmartPill
+//
+//  Created by Dennis da Silva Nunes on 11/11/14.
+//  Copyright (c) 2014 IFRN - Mobile School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPViewController : UIViewController
+
+@end
---
+++ SmartPill/SmartPill/SPViewController.m
@@ -0,0 +1,29 @@
+//
+//  SPViewController.m
+//  SmartPill
+//
+//  Created by Dennis da Silva Nunes on 11/11/14.
+//  Copyright (c) 2014 IFRN - Mobile School. All rights reserved.
+//
+
+#import "SPViewController.h"
+
+@interface SPViewController ()
+
+@end
+
+@implementation SPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
