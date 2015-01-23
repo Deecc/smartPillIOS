816bb7fc60f1941398009066b24ac6583739a99e
Dennis da Silva Nunes <nunesdennis@me.com>
2015-01-23 15:05:11 -0300

    Ajustes para evitar conflito de versões.

--- New Generation SmartPill/SmartPill/Controller/SPReminderViewController.m
+++ New Generation SmartPill/SmartPill/Controller/SPReminderViewController.m
@@ -7,113 +7,146 @@
 //
 
 #import "SPReminderViewController.h"
+#import "SPAppDelegate.h"
+#import "SPUserHandler.h"
+#import "User.h"
+#import "Medicine.h"
+#import "Reminder.h"
+#import "SPNewReminderViewController.h"
+#import "SPReminderDetailsViewController.h"
 
 @interface SPReminderViewController ()
+
+@property (strong,nonatomic) NSMutableArray * medicines;
+@property (strong,nonatomic) NSMutableArray * reminders;
 
 @end
 
 @implementation SPReminderViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
+- (NSManagedObjectContext *)managedObjectContext
+{
+    NSManagedObjectContext *context = nil;
+    SPAppDelegate * delegate = [[UIApplication sharedApplication] delegate];
+    if (delegate.managedObjectContext) {
+        context = delegate.managedObjectContext;
+    }
+    return context;
+}
+
+- (NSMutableArray *)medicines
+{
+    User * user = [self getCurrentDatabaseUser];
+    _medicines = [[user.medicine allObjects]mutableCopy];
+    return _medicines;
+}
+
+
+- (NSMutableArray *)reminders
+{
+    _reminders = [@[] mutableCopy];
+    for (Medicine * med in self.medicines) {
+        [_reminders addObject:med.reminder];
+    }
+    return _reminders;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+- (SPUser*)getCurrentUser{
+    SPAppDelegate* delegate = [[UIApplication sharedApplication] delegate];
+    return delegate.currentUser;
+}
+
+- (User*)getCurrentDatabaseUser{
+    SPUser * currentUser = [self getCurrentUser];
+    NSArray * arrayOfDataBaseUsers = [SPUserHandler checkPresenceToReturnUserLocally:currentUser OnDataBase:[self managedObjectContext]];
+    for (User *dataBaseUser in arrayOfDataBaseUsers) {
+        if ([dataBaseUser.email isEqualToString:currentUser.email])
+        {
+            return dataBaseUser;
+        }
+    }
+    return nil;
+}
+
+#pragma mark - Table view data source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return self.reminders.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
     
     // Configure the cell...
+    NSManagedObject *reminder = [self.reminders objectAtIndex:indexPath.row];
+    [cell.textLabel setText:[NSString stringWithFormat:@"%@", [reminder valueForKey:@"reminder_schedule"]]];
+    [cell.detailTextLabel setText:[[reminder valueForKey:@"medicine"]valueForKey:@"name"]];
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
-// Override to support editing the table view.
+
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        // Delete object from database
+        [context deleteObject:[self.reminders objectAtIndex:indexPath.row]];
+        
+        NSError *error = nil;
+        if (![context save:&error]) {
+            NSLog(@"Can't Delete! %@ %@", error, [error localizedDescription]);
+            return;
+        }
+        
+        // Remove Medicine from table view
+        [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    [self performSegueWithIdentifier:@"reminderDetailsSegue" sender:nil];
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([[segue identifier] isEqualToString:@"reminderDetailsSegue"]) {
+        Reminder * selectedReminder = [self.reminders objectAtIndex:[[self.tableView indexPathForSelectedRow] row]];
+        SPReminderDetailsViewController * reminderDetailsVC = segue.destinationViewController;
+        reminderDetailsVC.reminder = selectedReminder;
+    }else if ([[segue identifier] isEqualToString:@"newReminder"]) {
+        //SPNewReminderViewController * newReminderVC = segue.destinationViewController;
+    }
 }
-*/
 
 @end
