eb3e2e3a55c27b60e0ff3473dfdea3abc8888ac2
decK <deck@Mac-Pro-de-decK.local>
2015-02-12 11:18:56 -0800

    Criação de telas para histórico de medicamentos

---
+++ New Generation SmartPill/SmartPill/SPMedicineHistoryViewController.h
@@ -0,0 +1,16 @@
+//
+//  SPMedicineHistoryViewController.h
+//  SmartPill
+//
+//  Created by decK on 12/02/15.
+//  Copyright (c) 2015 IFRN - Mobile School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SPMedicineHistoryViewController : NSObject 
+
+
+
+@end
---
+++ New Generation SmartPill/SmartPill/SPMedicineHistoryViewController.m
@@ -0,0 +1,13 @@
+//
+//  SPMedicineHistoryViewController.m
+//  SmartPill
+//
+//  Created by decK on 12/02/15.
+//  Copyright (c) 2015 IFRN - Mobile School. All rights reserved.
+//
+
+#import "SPMedicineHistoryViewController.h"
+
+@implementation SPMedicineHistoryViewController
+
+@end
