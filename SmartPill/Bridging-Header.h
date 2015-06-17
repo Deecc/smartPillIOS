be6b92670cc3ca37a6efa8810256034072502d12
Dennis da Silva Nunes <nunesdennis@me.com>
2015-06-17 20:19:55 -0300

    Bridge configurado corretamente, .plist configurado para o facebook, sdk do facebook dentro do projeto.

---
+++ SmartPill/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  SmartPill
+//
+//  Created by Dennis da Silva Nunes on 17/06/15.
+//  Copyright (c) 2015 Dennis da Silva Nunes. All rights reserved.
+//
+
+#ifndef SmartPill_Bridging_Header_h
+#define SmartPill_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif
